133ed69cb603d6a01932c7492b12abf192ed3ea8
Adetola Olukoya <adetola442@gmail.com>
2024-09-16 19:36:37 -0500

    Update Aircraftdata

--- Aircraftdata.js
+++ Aircraftdata.js
@@ -1,3 +1,4 @@
+
 export const featuredAircraft =[
     {
         id: '1',
@@ -5,8 +6,8 @@ export const featuredAircraft =[
         name: '2006 LEARJET 45XR',
         seating: '9',
         airframe_hours: '3140.8',
-        image: 'src/featured_assests/2006learjet.png',
-        image_interior: 'src/assets/N74PT-0122.jpg'
+        image: './src/featured_assests/2006learjet.png',
+        image_interior: './src/assets/N74PT-0122.jpg'
     },
     {
         id: '2',
@@ -14,8 +15,8 @@ export const featuredAircraft =[
         name: '2018 GULFSTREAM G650ER',
         seating: '13',
         airframe_hours: '2229.9',
-        image: 'src/featured_assests/2018Gulfstream.png',
-        image_interior: 'src/assets/N2E-WebRes-3.png',
+        image: './src/featured_assests/2018Gulfstream.png',
+        image_interior: './src/assets/N2E-WebRes-3.png',
     },
     {
         id: '3',
@@ -23,8 +24,8 @@ export const featuredAircraft =[
         name: '2014 GULFSTREAM G650ER',
         seating: '14',
         airframe_hours: '4876.7',
-        image: 'src/featured_assests/2014Gulfstream.png',
-        image_interior: 'src/assets/jet-25.png',
+        image: './src/featured_assests/2014Gulfstream.png',
+        image_interior: './src/assets/jet-25.png',
     },
     {
         id: '4',
@@ -32,8 +33,8 @@ export const featuredAircraft =[
         name: '2006 BOMBARDIER GLOBAL',
         seating: '13',
         airframe_hours: '7281.6',
-        image: 'src/featured_assests/2006Bombardier.png',
-        image_interior: 'src/assets/32.png',
+        image: './src/featured_assests/2006Bombardier.png',
+        image_interior: './src/assets/32.png',
     },
     {
         id: '5',
@@ -41,8 +42,8 @@ export const featuredAircraft =[
         name: '2005 GULFSTREAM G550',
         seating: '16',
         airframe_hours: '9842.0',
-        image: 'src/featured_assests/2005Gulfstream.png',
-        image_interior: 'src/assets/N885GM-WebRes-2.png',
+        image: './src/featured_assests/2005Gulfstream.png',
+        image_interior: './src/assets/N885GM-WebRes-2.png',
     },
 ]
 
@@ -56,8 +57,8 @@ export const allAircraft =[
         name: '2006 LEARJET 45XR',
         seating: '9',
         airframe_hours: '3140.8',
-        image: 'src/featured_assests/2006learjet.png',
-        image_interior: 'src/assets/N74PT-0122.jpg'
+        image: './src/featured_assests/2006learjet.png',
+        image_interior: './src/assets/N74PT-0122.jpg'
     },
     {
         id: '2',
@@ -65,8 +66,8 @@ export const allAircraft =[
         name: '2018 GULFSTREAM G650ER',
         seating: '13',
         airframe_hours: '2229.9',
-        image: 'src/featured_assests/2018Gulfstream.png',
-        image_interior: 'src/assets/N2E-WebRes-3.png',
+        image: './src/featured_assests/2018Gulfstream.png',
+        image_interior: './src/assets/N2E-WebRes-3.png',
     },
     {
         id: '3',
@@ -74,8 +75,8 @@ export const allAircraft =[
         name: '2014 GULFSTREAM G650ER',
         seating: '14',
         airframe_hours: '4876.7',
-        image: 'src/featured_assests/2014Gulfstream.png',
-        image_interior: 'src/assets/jet-25.png',
+        image: './src/featured_assests/2014Gulfstream.png',
+        image_interior: './src/assets/jet-25.png',
     },
     {
         id: '4',
@@ -83,8 +84,8 @@ export const allAircraft =[
         name: '2006 BOMBARDIER GLOBAL',
         seating: '13',
         airframe_hours: '7281.6',
-        image: 'src/featured_assests/2006Bombardier.png',
-        image_interior: 'src/assets/32.png',
+        image: './src/featured_assests/2006Bombardier.png',
+        image_interior: './src/assets/32.png',
     },
     {
         id: '5',
@@ -92,8 +93,8 @@ export const allAircraft =[
         name: '2005 GULFSTREAM G550',
         seating: '16',
         airframe_hours: '9842.0',
-        image: 'src/featured_assests/2005Gulfstream.png',
-        image_interior: 'src/assets/N885GM-WebRes-2.png',
+        image: './src/featured_assests/2005Gulfstream.png',
+        image_interior: './src/assets/N885GM-WebRes-2.png',
     },
 
     {
@@ -102,8 +103,8 @@ export const allAircraft =[
         name: '1999 GULFSTREAM GV',
         seating: '17',
         airframe_hours: '11,900.8',
-        image: 'src/assets/_7R41983 2.png',
-        image_interior: 'src/assets/_7R42028-1.png',
+        image: './src/assets/_7R41983 2.png',
+        image_interior: './src/assets/_7R42028-1.png',
     },
 
     {
@@ -112,8 +113,8 @@ export const allAircraft =[
         name: '2021 BOEING BBJ MAX 8',
         seating: '17',
         airframe_hours: '672.0',
-        image: 'src/assets/Avjet-2021-Max-8-2-1224x695.jpg.webp',
-        image_interior: 'src/assets/Avjet-2021-Max-8-3-1224x695.jpg.webp',
+        image: './src/assets/Avjet-2021-Max-8-2-1224x695.jpg.webp',
+        image_interior: './src/assets/Avjet-2021-Max-8-3-1224x695.jpg.webp',
     },
 
     {
@@ -122,8 +123,8 @@ export const allAircraft =[
         name: '2010 BOEING BUSINESS JET',
         seating: '19',
         airframe_hours: '4183.3',
-        image: 'src/assets/2010-BOEING-BUSINESS-JET-BG.png',
-        image_interior: 'src/assets/2010 BOEING BUSINESS JET_Interior Wide Angle 2.png',
+        image: './src/assets/2010-BOEING-BUSINESS-JET-BG.png',
+        image_interior: './src/assets/2010 BOEING BUSINESS JET_Interior Wide Angle 2.png',
     },
 
 ]
